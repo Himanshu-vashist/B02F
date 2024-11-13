d1ddc27f8625726cd8f08c6e252733abec7f1021
Himanshu Kumar <143962962+Himanshu-vashist@users.noreply.github.com>
2024-11-13 19:00:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// App.jsx
+
 import React from 'react';
 import Navbar from './components/navbar';
 import './App.css';
@@ -14,17 +14,17 @@ function App() {
           backgroundImage: "url('/public/images/image 1.png')",
         }}
       >
-        {/* Add a dark overlay for better text contrast */}
+        
         <div className="absolute inset-0 bg-black bg-opacity-40"></div>
 
         {/* Container for the content */}
         <div className="relative container mx-auto flex items-center justify-center h-full px-4">
           <div className="text-center text-white">
-            {/* Title */}
+            
             <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
               Navigate Your Academic Journey with Confidence
             </h1>
-            {/* Subtitle */}
+      
             <h2 className="mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl">
               Providing high-quality and comprehensive assistance with assignments
               and thesis writing for students at all academic levels
